b9a5a26d39f1653881aab4262bf6d07fe463920d
PTune03 <petr.matveev2003@gmail.com>
2021-12-07 00:17:25 +0300

    Commit #2

--- actions/actions.py
+++ actions/actions.py
@@ -1,25 +1,34 @@
-
+from ruamel.yaml import YAML
+import logging
+import base64
+import os
+import urllib.parse as urlparse
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, FollowupAction, EventType, Restarted
+from rasa_sdk.forms import FormAction, REQUESTED_SLOT
+from pathlib import Path
+from typing import Dict, Text, Any, List, Optional
 from rasa_sdk.executor import CollectingDispatcher
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-from datetime import datetime as dt
-from typing import Any, Text, Dict, List
+ruamel_yaml = YAML(typ='safe')
+domain_data = ruamel_yaml.load(Path('domain.yml'))
 
 
-class ActionShowTime(Action):
-
-    def name(self) -> Text:  # регистрация имени действия
-        return "action_show_time"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # при вызове действия, возвращать ответ с текущим временем:
-        text = tracker.latest_message['text']
-        utter = f'На ваш вопрос "{text}" отвечу: {dt.now().strftime("%H:%M")}'
-        dispatcher.utter_message(text=utter)
-        # dispatcher.utter_message(text=f'Сейчас {dt.now().strftime("%H:%M")}')
-
-        return []
+# class ActionShowTime(Action):
+#
+#     def name(self) -> Text:  # регистрация имени действия
+#         return "action_show_time"
+#
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         # при вызове действия, возвращать ответ с текущим временем:
+#         text = tracker.latest_message['text']
+#         utter = f'На ваш вопрос "{text}" отвечу: {dt.now().strftime("%H:%M")}'
+#         dispatcher.utter_message(text=utter)
+#         # dispatcher.utter_message(text=f'Сейчас {dt.now().strftime("%H:%M")}')
+#
+#         return []
 
 
 class ActionHelloWorld(Action):
@@ -32,7 +41,35 @@ class ActionHelloWorld(Action):
 
         return []
 
+class ActionFAQ(Action):
 
+    def name(self) -> Text:
+        return "action_faq"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain) -> List[EventType]:
+        text = tracker.latest_message['text']
+        text = text.lower()
+        intent = tracker.get_intent_of_latest_message()
+        if intent in domain_data.get('answers', {}):
+            message = domain_data['answers'][intent]['text']
+            utter = f'На ваш вопрос "{text}" отвечу: {message}'
+        if not intent:
+            return [FollowupAction('action_classification')]
+
+        dispatcher.utter_message(text=utter)
+
+        return [Restarted()]
+
+
+class ActionClassification(Action):
+
+    def name(self) -> Text:
+        return "action_classification"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain) -> List[EventType]:
+        dispatcher.utter_message(text="Работает!")
+
+        return [Restarted()]
 # This files contains your custom actions which can be used to run
 # custom Python code.
 #
